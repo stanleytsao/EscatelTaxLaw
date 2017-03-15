76e47c502b3e4aad03ee1970a4fb33fc293ed7bf
Stanley Tsao <stsao002@gmail.com>
2017-03-15 11:14:12 -0700

    transfer backend to new branch

--- website/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'public/index.html', context=None)
